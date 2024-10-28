1b1d38fa85848f75f01acc4ec13a85cc29a6628f
Nazmi05 <narikoscarlet@gmail.com>
2024-10-28 16:40:27 +0600

    Add files via upload

---
+++ hw- forEach/main.js
@@ -0,0 +1,50 @@
+
+    /*const task1=function(){
+  const string=['яблоко', 'банан', 'апельсин']
+ string.forEach(item=>{
+     console.log(item)
+ }
+ )
+}
+ */
+
+/*const task2=function(){
+ const num= [1, 2, 3]
+
+ num.forEach(item=>{
+    console.log(item*2);
+ })
+}
+*/
+
+ 
+/*const task3=function(){
+const sum=[5, 10, 15]
+ let Sum=0
+sum.forEach((item)=>{
+ console.log(Sum+=item)
+})
+ console.log(Sum);
+}
+ */
+
+/*const task4=function(){
+
+    const  text=['привет', 'мир']
+   
+    text.forEach((item)=>{
+   console.log(item.toUpperCase());
+    
+    })
+   }
+   
+*/
+/*const task5=function(){
+    const five=[2, 5, 8, 1, 10]
+     five.forEach(item=>{
+       if(item>5){
+           console.log(item);
+       }
+     })
+    }
+     */
